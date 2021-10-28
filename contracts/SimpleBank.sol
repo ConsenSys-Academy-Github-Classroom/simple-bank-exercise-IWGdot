2c082461d548c05fd712deb9a2161c888f05a0fb
IWGdot <80074821+IWGdot@users.noreply.github.com>
2021-10-28 10:55:57 +0800

    Update SimpleBank.sol

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -100,14 +100,14 @@ contract SimpleBank {
       // return the user's balance.
       if (enrolled[msg.sender]) {
       // 1. Use a require expression to guard/ensure sender has enough funds
-        require(balances[msg.sender] >= withdrawAmount, "Not enough funds")'
+        require(balances[msg.sender] >= withdrawAmount, "Not enough funds");
       // 2. Transfer Eth to the sender and decrement the withdrawal amount from
       //    sender's balance
         balances[msg.sender] -= withdrawAmount;
         msg.sender.transfer(withdrawAmount);
       // 3. Emit the appropriate event for this message
         emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
-        }
+      }
     return balances[msg.sender];
     }
 }
